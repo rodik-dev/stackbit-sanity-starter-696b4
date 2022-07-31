e2a36e4b8ea85ba823aff43f1928b05aca4e8bfb
Stackbit Code Editor <projects@stackbit.com>
2022-07-31 20:50:17 +0300

    safePrefix.js: removed

--- src/utils/safePrefix.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-const withPrefix = require('./withPrefix').default;
-
-export default function safePrefix(url) {
-    if (_.startsWith(url, '#') || _.startsWith(url, 'http')) {
-        return url;
-    }
-    return withPrefix(url);
-}
